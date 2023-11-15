f71d2d99f0ce0616b9e2ac4256e00fb9a675d791
Vinicius Mariano Afonso <vinicius.mariano.afonso@gmail.com>
2023-11-15 20:16:53 -0300

    basic OOP

--- POO/conta.py
+++ POO/conta.py
@@ -1,16 +1,47 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo        #Atributos privados
+        self.__limite = limite
         
-    def depositar(self, valor):
-        self.saldo += valor
+    def deposita(self, valor):
+        self.__saldo += valor
     
-    def retirar(self, valor):
-        self.saldo -= valor
+    def __pode_sacar(self, valor): #Método privado
+        pass
+
+    def sacar(self, valor):
+        if(valor <= (self.__saldo + self.__limite)):
+            self.__saldo -= valor
+        else:
+            print("Valor + limite insuficiente")
+
+    def transferir(self, destino, valor):
+        self.sacar(valor)
+        destino.deposita(valor)    
     
-    def get_extrato(self, valor):
-        print(f"R$ {self.saldo}")
+    @property                   #"Getters" e "Setters"
+    def numero(self):
+        return self.__numero
+    
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @property
+    def saldo(self):
+        return f"R$ {self.__saldo}"
+
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
